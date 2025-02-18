1624615c7cfeac975ff218eafa74099e996e0fe6
ArthurFlag <arthur.flageul-marquez@aiven.io>
2025-02-18 12:45:25 +0100

    add: initial website

---
+++ mysite/public/searchindex.en.js
@@ -0,0 +1,58 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "Log",
+    "uri": "/log/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Log",
+    "content": "Test",
+    "description": "Test",
+    "tags": [],
+    "title": "Getting started",
+    "uri": "/log/first-day/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Log \u003e  Getting started",
+    "content": "Test",
+    "description": "Test",
+    "tags": [],
+    "title": "Install",
+    "uri": "/log/first-day/install/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Log",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Second Day",
+    "uri": "/log/second-day/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Log",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Third Day",
+    "uri": "/log/third-day/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
